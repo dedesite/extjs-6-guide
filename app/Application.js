e997b1926a2d03de64f03bece26da0c766625a45
Andréas Livet <andreas.livet@gmail.com>
2017-11-29 17:43:22 +0100

    step-2 : create form elements

--- app/Application.js
+++ app/Application.js
@@ -12,7 +12,9 @@ Ext.define("App.Application", {
       header: {
         title: "La maison autonome"
       },
-      html: "Hello World!",
+      layout: {
+        type: "vbox"
+      },
       padding: 20,
       items: [
         {
@@ -21,9 +23,67 @@ Ext.define("App.Application", {
           items: [
             {
               xtype: "button",
-              text: "Soleil"
+              text: "Solaire"
             }
           ]
+        },
+        {
+          xtype: "panel",
+          layout: "form",
+          items: [
+            {
+              xtype: "numberfield",
+              label: "Nombre de W/h consommés /j",
+              reference: "nbWattsHourConsumption",
+              width: 400,
+              value: 200,
+              minValue: 0,
+              maxValue: 50000
+            },
+            {
+              xtype: "numberfield",
+              label: "Nb. de jours d'autonomie",
+              reference: "nbAutonomyDays",
+              width: 400,
+              value: 3,
+              minValue: 1,
+              maxValue: 6
+            },
+            {
+              xtype: "checkboxfield",
+              label: "Utilisation d'un onduleur ?",
+              reference: "hasInverter"
+            },
+            {
+              xtype: "numberfield",
+              label: "Profondeur maximale de décharge des batteries",
+              reference: "maxBatteryUnloadDepth",
+              value: 50,
+              minValue: 0,
+              maxValue: 90
+            },
+            {
+              xtype: "spinnerfield",
+              label: "Voltage des batteries",
+              reference: "batteryVoltage",
+              value: 12,
+              minValue: 6,
+              maxValue: 24,
+              stepValue: 6
+            },
+            {
+              xtype: "numberfield",
+              label: "Capacité des batteries en Ah",
+              reference: "batteryCapacity",
+              value: "100",
+              minValue: 10,
+              maxValue: 1000
+            }
+          ]
+        },
+        {
+          xtype: "label",
+          html: "<strong>Nombre de batteries nécessaires : 4</strong>"
         }
       ]
     });

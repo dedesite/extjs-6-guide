7e58f73a8a70b0fba9cf91ee1cea56a1f30798b1
Andréas Livet <andreas.livet@gmail.com>
2017-11-30 12:04:34 +0100

    step-3 : Move batteryCalculator to a separate view

--- app/Application.js
+++ app/Application.js
@@ -16,35 +16,6 @@ Ext.define("App.Application", {
         type: "vbox"
       },
       padding: 20,
-      viewModel: {
-        formulas: {
-          updateBatteryNumbers: {
-            bind: {
-              nbWattsHourConsumption: "{nbWattsHourConsumption.value}",
-              nbAutonomyDays: "{nbAutonomyDays.value}",
-              hasInverter: "{hasInverter.checked}",
-              maxBatteryUnloadDepth: "{maxBatteryUnloadDepth.value}",
-              batteryVoltage: "{batteryVoltage.value}",
-              batteryCapacity: "{batteryCapacity.value}"
-            },
-            get(params) {
-              const lineLoss = 0.97; // Appromimation
-              const totalLoss = params.hasInverter ? 0.9 * lineLoss : lineLoss;
-              const consumptionPerDay =
-                params.nbWattsHourConsumption * totalLoss;
-              const consumptionToCovert =
-                consumptionPerDay * params.nbAutonomyDays;
-              const capacityNeeded =
-                consumptionToCovert / (params.maxBatteryUnloadDepth / 100);
-              const ampHourNeeded = capacityNeeded / params.batteryVoltage;
-              const numBatteryNeeded = Math.ceil(
-                ampHourNeeded / params.batteryCapacity
-              );
-              return numBatteryNeeded;
-            }
-          }
-        }
-      },
       items: [
         {
           xtype: "toolbar",
@@ -57,65 +28,7 @@ Ext.define("App.Application", {
           ]
         },
         {
-          xtype: "panel",
-          layout: "form",
-          items: [
-            {
-              xtype: "numberfield",
-              label: "Nombre de W/h consommés /j",
-              reference: "nbWattsHourConsumption",
-              width: 400,
-              value: 200,
-              minValue: 0,
-              maxValue: 50000
-            },
-            {
-              xtype: "numberfield",
-              label: "Nb. de jours d'autonomie",
-              reference: "nbAutonomyDays",
-              width: 400,
-              value: 3,
-              minValue: 1,
-              maxValue: 6
-            },
-            {
-              xtype: "checkboxfield",
-              label: "Utilisation d'un onduleur ?",
-              reference: "hasInverter"
-            },
-            {
-              xtype: "numberfield",
-              label: "Profondeur maximale de décharge des batteries",
-              reference: "maxBatteryUnloadDepth",
-              value: 50,
-              minValue: 0,
-              maxValue: 90
-            },
-            {
-              xtype: "spinnerfield",
-              label: "Voltage des batteries",
-              reference: "batteryVoltage",
-              value: 12,
-              minValue: 6,
-              maxValue: 24,
-              stepValue: 6
-            },
-            {
-              xtype: "numberfield",
-              label: "Capacité des batteries en Ah",
-              reference: "batteryCapacity",
-              value: "100",
-              minValue: 10,
-              maxValue: 1000
-            }
-          ]
-        },
-        {
-          xtype: "label",
-          bind: {
-            html:
-              "<strong>Nombre de batteries nécessaires : {updateBatteryNumbers}</strong>"
-          }
+          xtype: "batterycalculator"
         }
       ]
     });
